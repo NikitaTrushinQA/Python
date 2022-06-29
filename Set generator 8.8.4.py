e5c0a0f3972c9fa80930987de3a626102071609d
NikitaTrushinQA <106978180+NikitaTrushinQA@users.noreply.github.com>
2022-06-29 16:49:01 +0300

    add Set generator 8.8.4.py @NikitaTrushinQA Co

---
+++ Set generator 8.8.4.py
@@ -0,0 +1,7 @@
+#Используя генератор множеств, дополните приведенный код,
+#так чтобы получить множество, содержащее первую букву каждого слова (в нижнем регистре) списка words.
+#Результат вывести на одной строке в алфавитном порядке, разделяя элементы одним символом пробела.
+
+words = ['Plum', 'Grapefruit', 'apple', 'orange', 'pomegranate', 'Cranberry', 'lime', 'Lemon', 'grapes', 'persimmon', 'tangerine', 'Watermelon', 'currant', 'Almond']
+myset={i[0].lower() for i in words}
+print(*sorted(myset))
