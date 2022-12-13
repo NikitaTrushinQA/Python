3fa390727b579ea5e14a6480888ceaf27cc9bf0a
NikitaTrushinQA <106978180+NikitaTrushinQA@users.noreply.github.com>
2022-12-13 20:05:33 +0300

    add codewars Counting Duplicates.py

---
+++ codewars Counting Duplicates.py
@@ -0,0 +1,27 @@
+#Count the number of Duplicates
+#Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+#Example
+#"abcde" -> 0 # no characters repeats more than once
+#"aabbcde" -> 2 # 'a' and 'b'
+#"aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+#"indivisibility" -> 1 # 'i' occurs six times
+#"Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+#"aA11" -> 2 # 'a' and '1'
+#"ABBA" -> 2 # 'A' and 'B' each occur twice
+
+
+
+def duplicate_count(text):
+    d = dict()
+    counter = 0
+    new_text = list(map(lambda x:x.lower(),list(text)))
+    for i in new_text:
+        d[i] = d.setdefault(i, 0) + 1
+    for key, value in d.items():
+        if value >= 2:
+            counter += 1
+    return counter
+
+
+print(duplicate_count('Indivisibilities'))
