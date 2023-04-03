9c187d5f7de5ed4f349e5eda2bb015bfdc53ebc8
NikitaTrushinQA <106978180+NikitaTrushinQA@users.noreply.github.com>
2023-04-03 22:36:57 +0300

    add stepik_pro 4.6.12.py

---
+++ stepik_pro 4.6.12.py
@@ -0,0 +1,8 @@
+#напишите программу, чтобы она сериализовала словарь dogs и записала результат в файл dogs.pkl
+
+import pickle
+
+dogs = {'Ozzy': 2, 'Filou': 7, 'Luna': 4, 'Skippy': 11, 'Barco': 13, 'Balou': 10, 'Laika': 15}
+
+with open('dogs.pkl', mode='wb') as file:
+    pickle.dump(dogs, file)
