e0794c4a816853474c2516469d995af7e68877b1
NikitaTrushinQA <106978180+NikitaTrushinQA@users.noreply.github.com>
2023-04-06 22:19:03 +0300

    add stepik_pro 6.4.9 namedtuple.py

---
+++ stepik_pro 6.4.9 namedtuple.py
@@ -0,0 +1,9 @@
+#Вам доступен именованный кортеж Game.
+#Дополните приведенный ниже код, чтобы он создал именованный кортеж типа ExtendedGame, имеющий те же поля,
+#что и Game, а также два дополнительных поля — release_date и price.
+
+from collections import namedtuple
+
+Game = namedtuple('Game', 'name developer publisher')
+
+ExtendedGame = namedtuple('ExtendedGame', [*Game._fields, 'release_date','price'])
