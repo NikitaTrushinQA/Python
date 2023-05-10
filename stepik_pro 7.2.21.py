97d925d05cff9e3af9337e3a74ca14506ff2956e
NikitaTrushinQA <106978180+NikitaTrushinQA@users.noreply.github.com>
2023-05-10 15:11:50 +0300

    add stepik_pro 7.2.21.py

---
+++ stepik_pro 7.2.21.py
@@ -0,0 +1,13 @@
+# Вам доступна программа, которая добавляет в список fifth пятую букву каждого слова из списка food.
+# Если слово не имеет пятой буквы, этой буквой считается символ _.
+# Дополните приведенный ниже код конструкцией try-except, чтобы он выполнился без ошибок.
+
+food = ['chocolate', 'chicken', 'corn', 'sandwich', 'soup', 'potatoes', 'beef', 'lox', 'lemonade']
+fifth = []
+
+for x in food:
+    try:
+        fifth.append(x[4])
+    except IndexError:
+        fifth.append('_')
+print(fifth)
