b3350996befcd6a0af5ffbf73a741a8f36f8c535
NikitaTrushinQA <106978180+NikitaTrushinQA@users.noreply.github.com>
2023-06-27 23:33:16 +0300

    add stepik_pro 9.1.8.py

---
+++ stepik_pro 9.1.8.py
@@ -0,0 +1,19 @@
+# Вам доступен словарь films, ключом в котором является название некоторого фильма,
+# а значением — словарь с оценками этого фильма от изданий imdb и kinopoisk.
+#
+# Дополните приведенный ниже код, чтобы он вывел название фильма с наименьшей средней оценкой.
+
+films = {'Spider-Man: No Way Home': {'imdb': 8.8, 'kinopoisk': 8.3},
+         'Don"t Look Up': {'imdb': 7.3, 'kinopoisk': 7.6},
+         'Encanto': {'imdb': 7.3, 'kinopoisk': 7.4},
+         'The Witcher': {'imdb': 8.2, 'kinopoisk': 7.3},
+         'Ghostbusters: Afterlife': {'imdb': 7.3, 'kinopoisk': 8},
+         'Harry Potter 20th Anniversary: Return to Hogwarts': {'imdb': 8.1, 'kinopoisk': 8.2},
+         'Shingeki no Kyojin': {'imdb': 9.0, 'kinopoisk': 8.3},
+         'The Matrix': {'imdb': 8.7, 'kinopoisk': 8.5},
+         'The Dark Knight': {'imdb': 9.0, 'kinopoisk': 8.5},
+         'The Shawshank Redemption': {'imdb': 9.3, 'kinopoisk': 9.1},
+         'Avengers: Endgame': {'imdb': 8.4, 'kinopoisk': 7.7}}
+
+result = min(films, key=lambda x: sum(films[x].values()))
+print(result)
