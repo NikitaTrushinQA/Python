952c52eb5bda9d7372e33b8416b9bc5401114c0e
NikitaTrushinQA <106978180+NikitaTrushinQA@users.noreply.github.com>
2023-10-22 23:29:10 +0300

    add stepik_pro 10.12.20.py

---
+++ stepik_pro 10.12.20.py
@@ -0,0 +1,16 @@
+# Вам доступна программа, которая выводит все обозначения полей шахматной доски в алфавитном порядке через пробел.
+#
+# Перепишите данную программу с использованием функции product(), чтобы она выполняла ту же задачу.
+#
+# Примечание 1. Начальная часть ответа выглядит так:
+#
+# a1 a2 a3 a4 a5 a6 a7 a8 b1 b2 ...
+
+from string import ascii_lowercase
+from itertools import product
+
+letters = ascii_lowercase[:8]
+digits = [1, 2, 3, 4, 5, 6, 7, 8]
+
+for field in product(letters,digits):
+    print(*field, sep='', end=' ')
